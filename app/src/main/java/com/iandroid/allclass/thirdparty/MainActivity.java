d07a344d17723f63b2413105f7f7af8aa169d65c
kunming.wang <461133899@qq.com>
2020-06-13 13:22:17 +0800

    [FEATURE #00]lib init

---
+++ app/src/main/java/com/iandroid/allclass/thirdparty/MainActivity.java
@@ -0,0 +1,14 @@
+package com.iandroid.allclass.thirdparty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
